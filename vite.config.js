0d7fbe5191f4a18e4a2044e08203f7ae94cb6d0c
Erick Amezcua <erickamezcua12@gmail.com>
2026-05-06 20:40:59 -0600

    Adds option to deploy to gituhub pages

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  // Cambia 'nombre-de-tu-repo' por el nombre real de tu repositorio en GitHub
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: "build", // Aquí indicas que la salida sea la carpeta 'build'
   },
 });
